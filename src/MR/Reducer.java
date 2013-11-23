d8940311ced01e0becc9f36933d18fb4a3e1c49d
Andrea Klein <alklein@alumni.stanford.edu>
2013-11-22 22:37:44 -0500

    implemented client wordcount reducer

--- src/MR/Reducer.java
+++ src/MR/Reducer.java
@@ -8,22 +8,22 @@ public abstract class Reducer {
     /*
     Reduce function to be implemented by client.
     */
-    public abstract String reduce(String sorted_filename);
+    public abstract String reduce(ArrayList<String> sorted_filename, ArrayList<String> sorted_chunkIDs);
 
     /*
     Takes a list of filenames. Each is an output from a mapper.
     Performs an external merge sort on the mapped data to create a single file named sorted_fileanme.
     */
-    private void external_merge_sort(ArrayList<String> mapped_filenames, String sorted_filename) {
+    private void external_merge_sort(ArrayList<String> mapped_filenames, ArrayList<String> mapped_chunkIDs) {
 	// TODO
     }
 
     /*
     Sorts and reduces mapped data.
     */
-    public String run(ArrayList<String> mapped_file_contents, String sorted_filename) {
-	external_merge_sort(mapped_file_contents, sorted_filename);
-	return reduce(sorted_filename);
+    public String run(ArrayList<String> mapped_filenames, ArrayList<String> mapped_chunkIDs, ArrayList<String> sorted_filenames, ArrayList<String> sorted_chunkIDs) {
+	external_merge_sort(mapped_filenames, mapped_chunkIDs);
+	return reduce(sorted_filenames, sorted_chunkIDs);
     }
 
 }
