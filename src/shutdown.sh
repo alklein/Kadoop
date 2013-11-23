91c04481d50e98b4c26ab7624aef1b900c0913c4
Andrea Klein <alklein@alumni.stanford.edu>
2013-11-23 13:40:42 -0500

    startup and shutdown scripts updated to include MR master and compute nodes

--- src/shutdown.sh
+++ src/shutdown.sh
@@ -1,8 +1,15 @@
-SHUTDOWN="ps aux | grep 'java DFS/NameNode' | awk '{print "'$2'"}' | xargs kill; exit"
+SHUTDOWN_NN="ps aux | grep 'java DFS/NameNode' | awk '{print "'$2'"}' | xargs kill; exit"
+SHUTDOWN_MR="ps aux | grep 'java MR/Master' | awk '{print "'$2'"}' | xargs kill; exit"
 USERNAME="andreakl"
 
-python print_master_node.py | while read N_NODE
+python print_master_node.py | while read NODE
 do
-    echo "${SHUTDOWN}" | ssh ${USERNAME}@${N_NODE} /bin/bash &
+    echo "${SHUTDOWN_NN}" | ssh ${USERNAME}@${NODE} /bin/bash &
     echo "NameNode has been shut down."
+done
+
+python print_master_node.py | while read NODE
+do
+    echo "${SHUTDOWN_MR}" | ssh ${USERNAME}@${NODE} /bin/bash &
+    echo "Master has been shut down."
 done
--- src/start.sh
+++ src/start.sh
@@ -7,6 +7,8 @@ HOME="DS/Kadoop/src"
 CD_SCRIPT="cd $HOME ; "
 NN_SCRIPT="java DFS/NameNode"
 DN_SCRIPT="java DFS/DataNode"
+MR_SCRIPT="java MR/Master"
+CN_SCRIPT="java MR/ComputeNode"
 
 python print_master_node.py | while read N_NODE
 do
@@ -18,4 +20,18 @@ sleep 3
 python print_worker_nodes.py | while read D_NODE
 do
     echo "${CD_SCRIPT} $DN_SCRIPT" | ssh $USERNAME@$D_NODE /bin/bash &
+done
+
+
+
+python print_master_node.py | while read M_NODE
+do
+    echo "${CD_SCRIPT} ${MR_SCRIPT}" | ssh ${USERNAME}@${M_NODE} /bin/bash &
+done
+
+sleep 3
+
+python print_worker_nodes.py | while read C_NODE
+do
+    echo "${CD_SCRIPT} $CN_SCRIPT" | ssh $USERNAME@$C_NODE /bin/bash &
 done
